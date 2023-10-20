8ed9ed6406e1e56f2740d153864a9840f8b64f4e
Sihwan Yu <tlghks1217@naver.com>
2023-10-20 17:43:54 +0900

    feat: 동적할당을 위한 resize 메서드 구현

--- Stack.java
+++ Stack.java
@@ -1,5 +1,7 @@
 import Interface_form.StackInterface;
 
+import java.util.Arrays;
+
 public class Stack<E> implements StackInterface<E> {
 
     private static final int DEFAULT_CAPACITY = 10;         // 기본 크기
@@ -17,6 +19,32 @@ public class Stack<E> implements StackInterface<E> {
     public Stack(int capacity) {
         this.array = new Object[capacity];
         this.size = 0;
+    }
+
+    // 동적할당의 위한 private 메서드
+    private void resize() {
+
+        if (Arrays.equals(array, EMPTY_ARRAY)) {        // 빈 배열의 경우
+            array = new Object[DEFAULT_CAPACITY];
+            return;
+        }
+
+        int arrayCapacity = array.length;                // 현재 용적 크기
+
+        if (size == arrayCapacity) {                     // 용적이 가득 찰 경우 새로운 사이즈 생성
+            int newSize = arrayCapacity * 2;
+
+            array = Arrays.copyOf(array, newSize);      // 배열복사
+            return;
+        }
+
+        if (size < (arrayCapacity / 2)) {                // 현재 용적의 절반 미만으로 데이터가 존재할 경우 용적 감소
+
+            int newCapacity = (arrayCapacity / 2);
+
+            array = Arrays.copyOf(array, Math.max(DEFAULT_CAPACITY, newCapacity));
+            return;
+        }
     }
 
     @Override
