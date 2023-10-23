53ef801b263f9f641a8e2efee85d7c8d291f5c4c
Sihwan Yu <tlghks1217@naver.com>
2023-10-23 22:51:48 +0900

    feat: Queue 클래스 생성

---
+++ PriorityQueue.java
@@ -0,0 +1,19 @@
+import Interface_form.QueueInterface;
+
+public class PriorityQueue extends QueueInterface {
+
+    @Override
+    public boolean offer(Object o) {
+        return false;
+    }
+
+    @Override
+    public Object poll() {
+        return null;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+}
